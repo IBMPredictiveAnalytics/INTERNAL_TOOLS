7efd5174bbcb24059f3d2873b41ff2d305ece1da
Jia Zhong Wu <wujz@cn.ibm.com>
2015-09-22 11:52:40 +0800

    Update CreateIndexForGithubWeb.py

--- CreateIndexForGitWeb_tool/CreateIndexForGithubWeb.py
+++ CreateIndexForGitWeb_tool/CreateIndexForGithubWeb.py
@@ -2,63 +2,75 @@ import urllib.request
 import urllib.error
 import json
 import re
+import math
 
 UNICODE_ERROR_LIST = []
 HTTP_ERROR_LIST = []
 VALUE_ERROR_LIST = []
 OTHER_ERROR_LIST = []
+MAX_REPO_NUM = 1000
+PER_PAGE = 100
+INDENT_SPACE = '    '
+
 
 def createIndexForWeb(index_for_web_path):
+    page_num = math.floor(MAX_REPO_NUM/PER_PAGE)
     #read name and desc info from api.github.com
-    api_url = "https://api.github.com/orgs/ibmpredictiveanalytics/repos?per_page=1000"
+    api_url = "https://api.github.com/orgs/ibmpredictiveanalytics/repos?page={0}&per_page={1}"
     raw_info_json_url = 'https://raw.githubusercontent.com/IBMPredictiveAnalytics/repos_name/master/info.json'
 
     #key_list for repository info.json
     key_list = ['type', 'provider', 'software', 'language', 'category', 'promotion']
-    indent_space = '    '
-
-    api_json_data = json.loads(urllib.request.urlopen(api_url).read().decode('utf-8'))
+       
     index_for_web_json = "{\n\"repository_index\":[\n"
     index_for_web = open(index_for_web_path,'w')
+    
+    for page_index in range(1, page_num+1):
+        cur_api_url = api_url.format(str(page_index),str(PER_PAGE))
+        api_json_data = json.loads(urllib.request.urlopen(cur_api_url).read().decode('utf-8'))
+        
+        if len(api_json_data) == 0:
+            break
 
-    for item in api_json_data:
-        repo_name = item['name']
-        #ignore .io repository
-        if('IBMPredictiveAnalytics.github.io' == repo_name):
-            continue
-        
-        repo_desc = item['description']
-        repo_push_time = item['pushed_at']
-        repo_info_json_url = re.sub('repos_name', repo_name, raw_info_json_url)
-        try:
-            repo_info_json = json.loads(urllib.request.urlopen(repo_info_json_url).read().decode('utf-8'))
-        except UnicodeDecodeError as e:
-            UNICODE_ERROR_LIST.append(repo_name+"  "+repo_push_time)
-            continue
-        except urllib.error.HTTPError as e:
-            HTTP_ERROR_LIST.append(repo_name+"  "+repo_push_time)
-            continue
-        except ValueError as e:
-            VALUE_ERROR_LIST.append(repo_name+"  "+repo_push_time)
-            continue
-        except Exception as e:
-            OTHER_ERROR_LIST.append(repo_name+"  "+repo_push_time)
-            continue
-        
-        json_item = indent_space+'{\n'
-        json_item += indent_space + indent_space + "\"repository\":" +"\"" + repo_name +"\",\n" 
-        json_item += indent_space + indent_space + "\"description\":" +"\"" + repo_desc +"\",\n"
-        json_item += indent_space + indent_space + "\"pushed_at\":" +"\"" + repo_push_time +"\",\n" 
-        
-        for key in key_list:
-            if type(repo_info_json[key]) == list:
-                val = repo_info_json[key][0]
-            else:
-                val = repo_info_json[key]
-            json_item += indent_space + indent_space + "\"" + key + "\":" + "\"" + val + "\",\n"
-        json_item = json_item[0:-2]+'\n'
-        json_item += indent_space + "},\n"  
-        index_for_web_json += json_item
+        for item in api_json_data:
+            repo_name = item['name']
+            #ignore .io repository
+            if('IBMPredictiveAnalytics.github.io' == repo_name):
+                continue
+            
+            repo_desc = item['description']
+            repo_push_time = item['pushed_at']
+            repo_info_json_url = re.sub('repos_name', repo_name, raw_info_json_url)
+            
+            try:
+                repo_info_json = json.loads(urllib.request.urlopen(repo_info_json_url).read().decode('utf-8'))
+            except UnicodeDecodeError:
+                UNICODE_ERROR_LIST.append(repo_name+"  "+repo_push_time)
+                continue
+            except urllib.error.HTTPError:
+                HTTP_ERROR_LIST.append(repo_name+"  "+repo_push_time)
+                continue
+            except ValueError:
+                VALUE_ERROR_LIST.append(repo_name+"  "+repo_push_time)
+                continue
+            except Exception:
+                OTHER_ERROR_LIST.append(repo_name+"  "+repo_push_time)
+                continue
+            
+            json_item = INDENT_SPACE+'{\n'
+            json_item += INDENT_SPACE + INDENT_SPACE + "\"repository\":" +"\"" + repo_name +"\",\n" 
+            json_item += INDENT_SPACE + INDENT_SPACE + "\"description\":" +"\"" + repo_desc +"\",\n"
+            json_item += INDENT_SPACE + INDENT_SPACE + "\"pushed_at\":" +"\"" + repo_push_time +"\",\n" 
+            
+            for key in key_list:
+                if type(repo_info_json[key]) == list:
+                    val = repo_info_json[key][0]
+                else:
+                    val = repo_info_json[key]
+                json_item += INDENT_SPACE + INDENT_SPACE + "\"" + key + "\":" + "\"" + val + "\",\n"
+            json_item = json_item[0:-2]+'\n'
+            json_item += INDENT_SPACE + "},\n"  
+            index_for_web_json += json_item
     index_for_web_json = index_for_web_json[0:-2]
     index_for_web_json += '\n]\n}'
     index_for_web.write(index_for_web_json)  
