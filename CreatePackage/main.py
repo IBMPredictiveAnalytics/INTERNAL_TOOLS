8aa49ef5b3df1333ab330345ecfed85af7860f05
wujiazhong <wujz@cn.ibm.com>
2015-11-02 15:21:14 +0800

    update

--- CreatePackage/main.py
+++ CreatePackage/main.py
@@ -10,16 +10,13 @@ from CreateLicenseIndex.createLicenseIndex import createLicenseIndex
 from CreateExtensionIndex.createExtensionIndex import createExtensionIndex 
 from CreateLangIndex.createLangIndex import createLangIndex 
 from common.packageTool import zip_dir
-
 import os,traceback,shutil
-
 LOG_DIR_NAME = 'log'
-PACAKAGE = "Package"
+PACAKAGE_NAME = "Package"
 LOG_NAME = 'main.log'
-ZIP_NAME = '{0}.zip'
 ABBR_PRODUCT = 'stats'
 WHOLE_PRO_NAME = 'Statistics'
-TAR = 'extension_index_resbundles'    
+ZIP_NAME = 'extension_index_resbundles'    
  
 if __name__ == '__main__':
     usage = "usage: %prog [options] arg1"  
@@ -34,7 +31,7 @@ if __name__ == '__main__':
     #    parser.error("Please input valid product name modeler or stats (casesensitive) for your index file")
     
     try: 
-        savePath = os.path.join(options.outdir,PACAKAGE)
+        savePath = os.path.join(options.outdir,PACAKAGE_NAME)
         if os.path.isdir(savePath):
             print("The output folder "+savePath+" already existed. Delete it to create a new one or exit?(y/n):")
             ch = input()
@@ -61,16 +58,17 @@ if __name__ == '__main__':
             # create index for extension
             mainLogger.info("'CreateExtensionIndex start...")
             ext_path = createExtensionIndex(savePath, ABBR_PRODUCT) 
-            print(ext_path)
             mainLogger.info("'CreateExtensionIndex complete...")
             
+            '''
+            Description: create two threads -- one aimed at creating license index; the other aimed at creating lang index
+            '''
             # create thread to get index for license
             mainLogger.info("'CreateLicenseIndex thread start...")
             runCreateLicenseIndex = runScriptThread(createLicenseIndex, savePath, ext_path)
             runCreateLicenseIndex.setDaemon(True)
             runCreateLicenseIndex.start()
 
-            
             # create thread to get index for language
             mainLogger.info("CreateLangIndex thread start ...")
             runCreateLangIndex = runScriptThread(createLangIndex, savePath, ext_path)
@@ -79,13 +77,20 @@ if __name__ == '__main__':
             
             runCreateLicenseIndex.join()
             runCreateLangIndex.join()
-            mainLogger.info("'CreateLicenseIndex thread complete...")
-            mainLogger.info("'CreateLangIndex thread complete...")
+            
+            if runCreateLicenseIndex.exitcode!=0:
+                raise Exception('Exception in ' + runCreateLicenseIndex.getName()+'\n'+runCreateLicenseIndex.exc_traceback)
+            
+            if runCreateLangIndex.exitcode!=0:
+                raise Exception('Exception in ' + runCreateLangIndex.getName()+'\n'+runCreateLangIndex.exc_traceback)
+            
+            mainLogger.info("'CreateLicenseIndex' thread complete...")
+            mainLogger.info("'CreateLangIndex' thread complete...")
             
             # start to compress files to ZIP
             if not runCreateLangIndex.isAlive() and not runCreateLicenseIndex.isAlive():
                 mainLogger.info("Start to compress files into package ...")
-                zippath = os.path.join(savePath,ZIP_NAME.format(TAR))
+                zippath = os.path.join(savePath,ZIP_NAME+'.zip')
                 zip_dir(savePath, zippath)
                 mainLogger.info("Compression complete ...")
             
@@ -98,4 +103,4 @@ if __name__ == '__main__':
         print(str(traceback.format_exc()))
     except IOError:
         print(str(traceback.format_exc()))
-        print("Need permission to create folder in "+options.outdir+" or the destination already contains a folder named '"+PACAKAGE+"'")        
+        print("Need permission to create folder in "+options.outdir+" or the destination already contains a folder named '"+PACAKAGE_NAME+"'")        
