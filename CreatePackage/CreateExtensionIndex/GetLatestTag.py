f0be3547b3e98380508646614e60693251660d99
Jia Zhong Wu <wujz@cn.ibm.com>
2016-03-28 13:58:04 +0800

    Update GetLatestTag.py

--- CreatePackage/CreateExtensionIndex/GetLatestTag.py
+++ CreatePackage/CreateExtensionIndex/GetLatestTag.py
@@ -5,9 +5,10 @@ Created on Dec 24, 2015
 '''
 import urllib.request,json
 
-RAW_URL = r'https://api.github.com/repos/ibmpredictiveanalytics/{0}/releases/latest?access_token=TOKEN'
+TOKEN = ''
+RAW_URL = r'https://api.github.com/repos/ibmpredictiveanalytics/{0}/releases/latest?access_token={1}'
 def getLatestTagNO(ext_name):
-    url = RAW_URL.format(ext_name)
+    url = RAW_URL.format(ext_name,TOKEN)
     tag_info = json.loads(urllib.request.urlopen(url, data=None, timeout=10).read().decode('utf-8'))
     tag_index = tag_info["tag_name"]
     return tag_index
